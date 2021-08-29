4dc4d6f2db90d72e649263e8250032dda1ea9e2b
Connor Douthat <7866089+cadouthat@users.noreply.github.com>
2021-08-29 13:15:25 -0700

    Fixed duration deepSleep test

--- shade-timer.ino
+++ shade-timer.ino
@@ -1,4 +1,8 @@
+#include <EEPROM.h>
+
 #define CHAIN_PULL_PIN 14
+
+constexpr uint64_t kOneHour_us = 60ull * 60 * 1000 * 1000;
 
 void toggleShade() {
   digitalWrite(CHAIN_PULL_PIN, LOW);
@@ -6,24 +10,40 @@ void toggleShade() {
   digitalWrite(CHAIN_PULL_PIN, HIGH);
 }
 
+void blink(int n) {
+  while (n-- > 0) {
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+  }
+}
+
 void setup() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(CHAIN_PULL_PIN, HIGH);
   pinMode(CHAIN_PULL_PIN, OUTPUT_OPEN_DRAIN);
 
-  Serial.begin(115200);
-  delay(500);
-
   toggleShade();
+
+  EEPROM.begin(1);
+  uint64_t duration_us;
+  if (EEPROM.read(0) > 0) {
+    duration_us = kOneHour_us * 9;
+    EEPROM.write(0, 0);
+    blink(1);
+  } else {
+    duration_us = kOneHour_us * 15;
+    EEPROM.write(0, 1);
+    blink(2);
+  }
+  while (!EEPROM.commit()) {
+    blink(10);
+  }
+
+  // Adjust duration to offset execution time.
+  ESP.deepSleep(duration_us - micros());
 }
 
-void loop() {
-  static unsigned long startTime = millis();
-  unsigned long duration = millis() - startTime;
-  if (duration >= 24 * 60 * 60 * 1000) {
-    startTime = millis();
-    toggleShade();
-  } else if (duration >= 9 * 60 * 60 * 1000) {
-    toggleShade();
-  }
-  delay(10000);
-}
+void loop() {}
